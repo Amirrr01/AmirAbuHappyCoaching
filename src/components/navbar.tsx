3dcd08518eba121b66a379fdfe1832f790b35f92
master+ <amirxonrustamov207@gmail.com>
2023-08-09 17:17:39 +0500

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+import { Flex } from "@mantine/core";
+import img from "../imgs/logo.svg";
+
+interface NavbarProps {}
+
+function NavbarMain({}: NavbarProps) {
+  return (
+    <nav className="navbar">
+      <Flex sx={{ justifyContent: "space-between" }} gap={1000}>
+        <img src={img} alt="" />
+        <Flex gap={30} align="center">
+          <a href="" className="navA">
+            Home
+          </a>
+          <a href="" className="navA">
+            Services
+          </a>
+          <a href="" className="navA">
+            About us
+          </a>
+          <a href="" className="navA">
+            Extra Salespage
+          </a>
+          <button className="navButton">Get your free guide now</button>
+        </Flex>
+      </Flex>
+    </nav>
+  );
+}
+export default NavbarMain;
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,6 @@
+import { NavbarMain } from "../../components";
+
+interface HomeProps {}
+
+const Home = () => <NavbarMain />;
+export default Home;
